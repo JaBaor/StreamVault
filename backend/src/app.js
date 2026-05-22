b9d068c1f9511c78a7de7af5a58b3730a0fd2764
Atomyst <danhuoc1234@gmail.com>
2026-05-22 08:58:23 +0700

    Set up app.js and server.js, add enviorment data

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();  //create the server app
+
+//Midlleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"))
+
+//Test route
+app.get("/", (req, res)=>{
+  res.json({
+    message: "Movie API running"
+  });
+});
+
+module.exports = app;
